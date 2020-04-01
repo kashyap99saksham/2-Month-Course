54a1d9df3603fe0de1bbdcf4129b6c56ed9f9dd2
unknown <kprincekashyap7@gmail.com>
2020-04-01 20:35:51 +0530

    dividing sub folders

---
+++ DAY 2/try.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int t;  cin>>t;
+    while(t--)
+    {
+        int size;   cin>>size;
+        int arr[size]
+        cout<<endl;        
+    }       
+}
+
+#
