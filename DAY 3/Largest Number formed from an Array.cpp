67bd64169c58a592c108f5a10e54c50e2da72c3d
unknown <kprincekashyap7@gmail.com>
2020-04-03 17:17:19 +0530

    Adding Code

---
+++ DAY 3/Largest Number formed from an Array.cpp
@@ -0,0 +1,29 @@
+// USING COMPARATOR FUNCTION
+#include <bits/stdc++.h>
+using namespace std;
+bool compare(string s1, string s2)
+{
+    return (stoi(s1+s2)>stoi(s2+s1));
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+    	int n;
+    	cin>>n;
+    	vector<string> v(n,"");
+    	for(int i=0;i<n;i++)
+        {
+    		cin>>v[i];
+    	}   
+    	sort(v.begin(), v.end(),compare);
+    	for(int i=0;i<n;i++)
+        {
+    		cout<<v[i];
+    	}
+    	cout<<endl;
+    }
+
+    return 0;
+}
